2a76e7888173f69183308ed1363d65a3679e91d1
ken <kenmugy@gmail.com>
2019-10-11 11:35:36 +0300

    added auth routes

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 const port = '1113';
 
+// import routes
+const authRoute = require('./routes/auth');
+
+// middleware
+app.use('/api/user', authRoute);
+
 app.listen(port, () => console.log(`listening on port ${port}`));
